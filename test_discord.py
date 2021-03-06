503e9bce7205a4c45f0ccd21240920ea8f5810d0
Nikhil Gupta <nikhilg2k@gmail.com>
2021-03-07 00:47:10 +0530

    Second

--- test_discord.py
+++ test_discord.py
@@ -1,40 +1,35 @@
 from discord.ext import commands
+from discord import embeds
 import asyncio
 
 token = 'ODE3NzM3NTM2MzEwNDc2ODIx.YEN3bQ.SXXDk7rw87AT_zDMmR1KleTrSy4'
 
 bot=commands.Bot(command_prefix='!')
 
-""" @client.event
-async def on_message(message):
-    channels=['nik_test']
-    if str(message.channel) in channels:
-        if message.content.find('!try') != -1:
-            countdown=10
-            
-            timer.content=str(countdown)
-            await message.channel.send(timer)
-            
-
- """
 allowed=True
 countdown=10
 answered=False
-@bot.command(name='try')
+answers={}
+channels=['team-1','team-2']
+@bot.command(name='start')
+@commands.has_role('Test_QM')
 async def trial(ctx):
     global allowed
     global countdown
     global answered
+    global answers
+    answers={}
     allowed=True
     answered = False
     message=await ctx.send(str(countdown))
+    print(type(message))
     while countdown>1:
         if answered == True:
             allowed = False
             countdown = 10
             answered = False
             break
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.9)
         await message.edit(content=str(countdown-1))
         countdown=countdown-1
     else:
@@ -47,12 +42,22 @@ async def trial(ctx):
 async def answer(ctx):
     global allowed
     global answered
+    global answers
     if allowed == True:
         answer=ctx.message.content[5:]
-        await ctx.send(f"seen. your answer is {answer}")
+        answers[str(ctx.channel)]=answer
+        await ctx.send(f"Seen. Your answer is {answer}")
         answered = True
     else:
-        await ctx.send('Answer closed')
+        await ctx.message.reply('Pounce closed')
 
-
+@bot.command(name='fetch')
+@commands.has_role('Test_QM')
+async def fetch_answers(ctx):
+    global answers
+    message=''
+    for i in answers.keys():
+        message=message+i+'        '+answers[i]+'\n'
+    await ctx.send(message)
+    
 bot.run(token)
