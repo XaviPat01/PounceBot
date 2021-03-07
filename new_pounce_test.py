422fa30f781476cbbda28b71c6f2a68f3f3af931
XaviPat01 <patni.avi@gmail.com>
2021-03-07 16:51:18 +0530

    PounceBot is functional!

--- new_pounce_test.py
+++ new_pounce_test.py
@@ -1,57 +1,106 @@
 from discord.ext import commands
+import requests
+import discord
 import asyncio
 import time
 
 #change token
-token = 'ODE3NzM3NTM2MzEwNDc2ODIx.YEN3bQ.SXXDk7rw87AT_zDMmR1KleTrSy4'
+token = 'ODE3NzQ3MjMwODM5Nzk5ODA4.YEOAdA.tBU1Hmr9tZkPsBSkFrmCNylpF8o'
 bot = commands.Bot(command_prefix='!')
 
-allowed = [False,False]
+""" allowed = [False,False]
 answered = [False,False]
 countdown = 0
 answers = {}
 #manual input of ids
 channels = ['team-1', 'team-2']
-channel_ids = [817940087206707240, 817940251623555142]
+channel_ids = [817940087206707240, 817940251623555142] """
+allowed=[]
+answered=[]
+text_channel_dict={}
+channel_ids=[]
+qnset=[]
+numteams=0
 
-@bot.command(name='start')
+@bot.command(name='new', help="Initialize the connections to the team channels")
+@commands.has_role("Test_QM")
+async def get_channel_ids(ctx):
+    global text_channel_dict
+    global channel_ids
+    global answered
+    global allowed
+    global numteams
+    text_channel_dict = {}
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            if 'team' in channel.name.lower():
+                text_channel_dict[channel.name] = channel.id
+    channel_ids = list(text_channel_dict.values())
+    print(channel_ids)
+    numteams=len(channel_ids)
+    print(numteams)
+    await ctx.send('Connections with teams established')
+    answered =[False]*numteams
+    allowed=[False]*numteams
+
+
+@bot.command(name="questions",help="To add the .txt file containing qns to be displayed on discord")
+@commands.has_role("Test_QM")
+async def qnreg(ctx):
+    global qnset
+    attachment_url = ctx.message.attachments[-1].url
+    file_request = requests.get(attachment_url)
+    contents=(file_request.text.split("\n"))
+    await ctx.send("Qns registered")
+    qnset=contents
+    # print(qnset)
+
+
+@bot.command(name="show",help="To send the question to every team channel, !show [question number]")
+@commands.has_role('Test_QM')
+async def qnshow(ctx,qno):
+    for id in channel_ids:
+        await bot.get_channel(id).send(qnset[int(qno)-1])
+
+
+@bot.command(name='start',help="Start the timer !start [time(in seconds)]")
 @commands.has_role('Test_QM')
 async def trial(ctx, arg):
     global allowed
     global countdown
     global answered
     global answers
+    global numteams
     answers = {}
-    allowed = [True,True]
-    answered = [False,False]
+    allowed = [True]*numteams
+    answered = [False]*numteams
     countdown = int(arg)
     t1 = time.time()
     messages =[]
     for id in channel_ids:
+        print(id)
         messages.append(await bot.get_channel(id).send(min([(countdown//5+1)*5,int(arg)])))
-    count_1=0
-    count_2=0
+    
+    count=[0]*numteams
+
     while countdown > 1:
         countdown = int(arg) - (time.time() - t1)
-
-        if answered[0] == True and count_1==0:
-            allowed[0] = False
-            countdown = int(arg)
-            count_1=1
-        if answered[1] == True and count_2==0:
-            allowed[1] = False
-            countdown = int(arg)
-            count_2=1
-            
-        else:
-            for message in messages:
-                await message.edit(content=(int(countdown)//5+1)*5)
+        for i in range(0,numteams):
+            if answered[i] == True and count[i]==0:
+                allowed[i] = False
+                countdown = int(arg)
+                count[i]=1
+                
+            else:
+                for message in messages:
+                    await message.edit(content=(int(countdown)//5+1)*5)
     else:
         for message in messages:
             await message.edit(content='TimeUp')
-        allowed = [False,False]
+        await ctx.send("Time's Up!")
+        allowed = [False]*numteams
         countdown = int(arg)
-        answered = [False,False]
+        answered = [False]*numteams
 
 @bot.command(name='ans', help="Write your pounce answer by mentioning !ans and then your answer")
 async def answer(ctx):
