a354a251d73db14f7d53944a73cff04d16a6639c
XaviPat01 <patni.avi@gmail.com>
2021-03-07 07:25:41 +0530

    Test_Bot working for a single channel

--- test_discord.py
+++ test_discord.py
@@ -7,13 +7,13 @@ token = 'ODE3NzM3NTM2MzEwNDc2ODIx.YEN3bQ.SXXDk7rw87AT_zDMmR1KleTrSy4'
 bot=commands.Bot(command_prefix='!')
 
 allowed=True
-countdown=10
+countdown=0
 answered=False
 answers={}
 channels=['team-1','team-2']
 @bot.command(name='start')
 @commands.has_role('Test_QM')
-async def trial(ctx):
+async def trial(ctx,arg):
     global allowed
     global countdown
     global answered
@@ -21,24 +21,28 @@ async def trial(ctx):
     answers={}
     allowed=True
     answered = False
-    message=await ctx.send(str(countdown))
+    countdown=int(arg)
+    message=await ctx.send(countdown)
     print(type(message))
     while countdown>1:
+        print(countdown,answered,allowed)
         if answered == True:
             allowed = False
-            countdown = 10
+            countdown = int(arg)
             answered = False
+            print("Answer received.")
             break
-        await asyncio.sleep(0.9)
-        await message.edit(content=str(countdown-1))
-        countdown=countdown-1
+        else:
+            await asyncio.sleep(0.9)
+            countdown-=1
+            await message.edit(content=countdown)
     else:
         await ctx.send('TimeUp')
         allowed = False
-        countdown = 10
+        countdown = int(arg)
         answered = False
         
-@bot.command(name='ans')
+@bot.command(name='ans', help="Write your pounce answer by mentioning !ans and then your answer")
 async def answer(ctx):
     global allowed
     global answered
@@ -51,13 +55,16 @@ async def answer(ctx):
     else:
         await ctx.message.reply('Pounce closed')
 
-@bot.command(name='fetch')
+@bot.command(name='fetch', help="To see the pounce answers of each team.")
 @commands.has_role('Test_QM')
 async def fetch_answers(ctx):
     global answers
     message=''
     for i in answers.keys():
         message=message+i+'        '+answers[i]+'\n'
-    await ctx.send(message)
+    if message=='':
+        await ctx.send("No team pounced")
+    else:
+        await ctx.send(message)
     
 bot.run(token)
